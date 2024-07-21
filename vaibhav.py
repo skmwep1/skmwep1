297b39c8d7db1b2c41e7810f3c1ed41f455731a3
skmwep1 <samratjob2@gmail.com>
2024-07-22 00:03:31 +0530

    Update vaibhav.py

--- vaibhav.py
+++ vaibhav.py
@@ -13,7 +13,7 @@ import pytz
 bot = telebot.TeleBot('7402344991:AAEO50oQLETKv8W7gwUSYI77BC7koLdy7X4')
 
 # Admin user IDs
-admin_id = ["2136214036","1770317273"]
+admin_id = ["2136214036","6685164810"]
 
 # File to store allowed user IDs
 USER_FILE = "users.txt"
